548b92fa22d98b2cd85f54b225335de6b4cb8c97
Ben Coleman <benc.uk@gmail.com>
2020-02-08 19:20:35 +0000

    now stores images in azure blob

--- functions/createFractal/index.js
+++ functions/createFractal/index.js
@@ -19,10 +19,13 @@ module.exports = async function (context, req) {
   let width =      parseInt(req.query.w || 600)
   let height =     parseInt(req.query.h || 400)
   let hue =        parseInt(req.query.hue || 120)
+  let hueLoops =   parseFloat(req.query.hueLoops || 4)  
   let sat =        parseInt(req.query.sat || 100)
   let brightness = parseInt(req.query.bright|| 150)
   let seedR =      parseFloat(req.query.juliar || 0.362)
   let seedI =      parseFloat(req.query.juliai || 0.370)
+  let innerBright = parseFloat(req.query.innerBright || 0)
+
   const ratio = width / height
   
   const canvas = createCanvas(width, height)
@@ -51,12 +54,11 @@ module.exports = async function (context, req) {
 
       // Choose colour of pixel
       if (iterations == maxiters) {
-        ctx.fillStyle = `rgb(0, 0, 0)`
+        ctx.fillStyle = `hsl(${hue}, ${sat}%, ${innerBright}%)`
       } else {
         // Outside the set, colour based on number of iterations
         let nIter = iterations / (maxiters - 1)
         let lumVal = nIter * brightness
-        let hueLoops = (zoom * zoom) / 15
         let hueVal = Math.cos(nIter * hueLoops) * hue
 
         ctx.fillStyle = `hsl(${hueVal}, ${sat}%, ${lumVal}%)`
--- functions/randomFractal/index.js
+++ functions/randomFractal/index.js
@@ -1,28 +1,67 @@
 const fs = require('fs')
-const request = require('request-promise-native')
+//const request = require('request-promise-native')
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+
+const account = process.env.AzureWebJobsStorage.split(';')[1].replace('AccountName=', '');
+const accountKey = process.env.AzureWebJobsStorage.split(';')[2].replace('AccountKey=', '')
+const container = "fractals"
 
 module.exports = async function (context, req) {
+  // Load points file and pick a random one
   points = JSON.parse( fs.readFileSync('points.json') )
   let point = points[Math.floor(Math.random() * points.length)]
 
   try {
     let createFractal = require('../createFractal');
-    let request = {
+
+    zoomDepth =  Math.random() > 0.5 ? 20 : 500
+    
+    // Create a fake request object with query params
+    let fractalRequest = {
       query: {
         w: parseInt(req.query.w || 400),
         h: parseInt(req.query.h || 300),
-        zoom: 3 + Math.random() * 40,
-        bright: 120,
+        zoom: parseInt(req.query.zoom || (3 + Math.random() * zoomDepth)),
+        bright: 80 + Math.random() * 40,
         hue: Math.random() * 255,
-        iter: 50 + Math.random() * 500,
+        iter: 50 + Math.random() * 100,
         r: point.r,
-        i: point.i
+        i: point.i,
+        hueLoops: Math.random() * 4,
+        innerBright: Math.random() * 50
       }
     }
 
-    let ctx = {}
-    await createFractal(ctx, request)
-    context.res = ctx.res
+    // Fake empty context
+    let fractalCtx = {}
+    // Call createFractal with our request, results will be put into ctx
+    await createFractal(fractalCtx, fractalRequest)
+    
+
+    const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+    const blobServiceClient = new BlobServiceClient(
+      `https://${account}.blob.core.windows.net`,
+      sharedKeyCredential
+    );
+
+    const containerClient = blobServiceClient.getContainerClient(container);
+    const content = fractalCtx.res.body;
+    const blobName = new Date().getTime() + ".png";
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    opt = {
+      blobHTTPHeaders: {
+        blobContentType: 'image/png'
+      }
+    }
+    await blockBlobClient.upload(content, content.length, opt);
+  
+    context.res = {
+      body: {
+        imageUrl: `https://${account}.blob.core.windows.net/${container}/${blobName}`,
+        fractal: fractalRequest.query
+      }
+    }
+
   } catch(err) {
     console.log(`### ERR ### ${err}`); 
   }
